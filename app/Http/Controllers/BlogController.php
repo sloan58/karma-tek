352be2344b8dbcb1327a1428eeaf93d88e3976c4
sloan58 <martin.sloan@karma-tek.com>
2015-08-21 12:16:59 -0400

    testing blog index layout

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -16,8 +16,7 @@ class BlogController extends Controller
     {
         $tag = $request->get('tag');
         $data = $this->dispatch(new BlogIndexData($tag));
-//        $layout = $tag ? Tag::layout($tag) : 'blog.layouts.index';
-        $layout = 'blog.layouts.index';
+        $layout = $tag ? Tag::layout($tag) : 'blog.layouts.index';
 
         return view($layout, $data);
     }
