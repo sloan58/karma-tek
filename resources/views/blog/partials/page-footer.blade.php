b01855c12fa659a310ddf47dc2be0788b1552cdc
sloan58 <martin.sloan@karma-tek.com>
2015-08-25 10:49:48 -0400

    remove disqus partial

--- resources/views/blog/partials/page-footer.blade.php
+++ resources/views/blog/partials/page-footer.blade.php
@@ -1,10 +1,3 @@
-<hr>
-<div class="container">
-  <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-    @include('blog.partials.disqus')
-  </div>
-</div>
-<hr>
 <footer>
   {{--<div class="container">--}}
     {{--<div class="row">--}}
